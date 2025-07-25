660c3924a489c2c75554a61807dfb72fd606fbc4
LMH-droid <1508844741@qq.com>
2025-07-25 21:39:37 +0800

    day01: HelloWorld + 变量 + 循环

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 21!");
+        int age = 18;
+        System.out.println("年龄 = " + age);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("循环第 " + i + " 次");
+        }
+    }
+}
